4e6f01e338461d0fa373d897ad9f6aeee5c4ce53
Shawn Leo <764139720@qq.com>
2023-04-14 18:36:01 +0800

    [feature]add gpu param to sh

--- run_cifar100.sh
+++ run_cifar100.sh
@@ -1,23 +1,23 @@
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.01   --wandb
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.005  --wandb
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.001  --wandb
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.0005 --wandb
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.0001 --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.01   --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.005  --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.001  --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.0005 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.0001 --gpu "$1" --wandb
 
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.2 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.2 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.2 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.2 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.2 --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.2 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.2 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.2 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.2 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.2 --gpu "$1" --wandb
 
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.5 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.5 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.5 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.5 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.5 --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.5 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.5 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.5 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.5 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.5 --gpu "$1" --wandb
 
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.8 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.8 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.8 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.8 --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.8 --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.8 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.8 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.8 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.8 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.8 --gpu "$1" --wandb
