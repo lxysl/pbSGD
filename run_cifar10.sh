133c35a6b6029234f9b918b34c7b43cc55849e04
Shawn Leo <764139720@qq.com>
2023-04-14 18:39:20 +0800

    [debug]add gpu param to sh

--- run_cifar10.sh
+++ run_cifar10.sh
@@ -1,23 +1,23 @@
-python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.01   --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.005  --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.001  --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.0005 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.0001 --gpu "$1" --wandb
+python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.01   --device "$1" --wandb
+python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.005  --device "$1" --wandb
+python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.001  --device "$1" --wandb
+python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.0005 --device "$1" --wandb
+python ./CIFAR10/main.py --optim Adam --arch densenet121 --lr 0.0001 --device "$1" --wandb
 
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.01   --gamma 0.2 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.005  --gamma 0.2 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.001  --gamma 0.2 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0005 --gamma 0.2 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0001 --gamma 0.2 --gpu "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.01   --gamma 0.2 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.005  --gamma 0.2 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.001  --gamma 0.2 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0005 --gamma 0.2 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0001 --gamma 0.2 --device "$1" --wandb
 
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.01   --gamma 0.5 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.005  --gamma 0.5 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.001  --gamma 0.5 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0005 --gamma 0.5 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0001 --gamma 0.5 --gpu "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.01   --gamma 0.5 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.005  --gamma 0.5 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.001  --gamma 0.5 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0005 --gamma 0.5 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0001 --gamma 0.5 --device "$1" --wandb
 
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.01   --gamma 0.8 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.005  --gamma 0.8 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.001  --gamma 0.8 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0005 --gamma 0.8 --gpu "$1" --wandb
-python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0001 --gamma 0.8 --gpu "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.01   --gamma 0.8 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.005  --gamma 0.8 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.001  --gamma 0.8 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0005 --gamma 0.8 --device "$1" --wandb
+python ./CIFAR10/main.py --optim pbAdam --arch densenet121 --lr 0.0001 --gamma 0.8 --device "$1" --wandb
--- run_cifar100.sh
+++ run_cifar100.sh
@@ -1,23 +1,23 @@
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.01   --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.005  --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.001  --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.0005 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.0001 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.01   --device "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.005  --device "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.001  --device "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.0005 --device "$1" --wandb
+python ./CIFAR100/main.py --optim Adam --arch resnext --lr 0.0001 --device "$1" --wandb
 
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.2 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.2 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.2 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.2 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.2 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.2 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.2 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.2 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.2 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.2 --device "$1" --wandb
 
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.5 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.5 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.5 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.5 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.5 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.5 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.5 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.5 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.5 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.5 --device "$1" --wandb
 
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.8 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.8 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.8 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.8 --gpu "$1" --wandb
-python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.8 --gpu "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.01   --gamma 0.8 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.005  --gamma 0.8 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.001  --gamma 0.8 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0005 --gamma 0.8 --device "$1" --wandb
+python ./CIFAR100/main.py --optim pbAdam --arch resnext --lr 0.0001 --gamma 0.8 --device "$1" --wandb
