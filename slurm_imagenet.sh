096934e1ad1e9eead100e2605133eab10b400122
lxy764139720 <lxy764139720@gmail.com>
2023-04-29 01:17:18 +0800

    [debug]slurm

--- slurm_imagenet.sh
+++ slurm_imagenet.sh
@@ -2,20 +2,19 @@
 #SBATCH --job-name=imagenet1
 #SBATCH --output=imagenet1.out.%j
 #SBATCH --error=vasp1.err.%j
-#SBATCH --partition=iirmas-gpu
-#SBATCH --nodes=4
+#SBATCH --partition=gpu
+#SBATCH --nodes=3
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
-#SBATCH	--cpus-per-task=4
+#SBATCH	--cpus-per-task=32
 #SBATCH --comment=hustHAIL
 
 echo "Activating conda environment..."
 source activate pt2
 
-echo "Setting Wandb API key..."
-export WANDB_API_KEY=42f4584252c42654c6dc375454d6cbbc098ddbc4
+# echo "Setting Wandb API key..."
+# export WANDB_API_KEY=42f4584252c42654c6dc375454d6cbbc098ddbc4
 
-srun python ./ImageNet/main.py --arch resnet50 --optim Adam --lr 0.001 -p 100 --wandb
-srun python ./ImageNet/main.py --arch resnet50 --optim AdamW --lr 0.001 -p 100 --wandb
-srun python ./ImageNet/main.py --arch resnet50 --optim Lion --lr 0.001 -p 100 --wandb
-srun python ./ImageNet/main.py --arch resnet50 --optim SGDM --lr 0.1 -p 100 --wandb
+srun python ./ImageNet/main.py --arch resnet50 --optim Adam --lr 0.001 -p 100 --wandb -j 32
+srun python ./ImageNet/main.py --arch resnet50 --optim pbAdam --lr 0.001 --gamma 0.8 -p 100 --wandb -j 32
+srun python ./ImageNet/main.py --arch resnet50 --optim SGDM --lr 0.1 -p 100 --wandb -j 32
