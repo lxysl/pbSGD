1640b5912e42716747d785230a9e4c836258de41
lxy764139720 <lxy764139720@gmail.com>
2023-05-01 12:35:40 +0800

    [refactor]tar -xf without v

--- ImageNet/extract_ILSVRC.sh
+++ ImageNet/extract_ILSVRC.sh
@@ -32,7 +32,7 @@ mkdir imagenet
 # Create train directory; move .tar file; change directory
 mkdir imagenet/train && mv ILSVRC2012_img_train.tar imagenet/train/ && cd imagenet/train
 # Extract training set; remove compressed file
-tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
+tar -xf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
 #
 # At this stage imagenet/train will contain 1000 compressed .tar files, one for each category
 #
@@ -40,7 +40,7 @@ tar -xvf ILSVRC2012_img_train.tar && rm -f ILSVRC2012_img_train.tar
 #   1. create directory with same name as .tar file
 #   2. extract and copy contents of .tar file into directory
 #   3. remove .tar file
-find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xvf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
+find . -name "*.tar" | while read NAME ; do mkdir -p "${NAME%.tar}"; tar -xf "${NAME}" -C "${NAME%.tar}"; rm -f "${NAME}"; done
 #
 # This results in a training directory like so:
 #
@@ -57,7 +57,7 @@ cd ../..
 # Extract the validation data and move images to subfolders:
 #
 # Create validation directory; move .tar file; change directory; extract validation .tar; remove compressed file
-mkdir imagenet/val && mv ILSVRC2012_img_val.tar imagenet/val/ && cd imagenet/val && tar -xvf ILSVRC2012_img_val.tar && rm -f ILSVRC2012_img_val.tar
+mkdir imagenet/val && mv ILSVRC2012_img_val.tar imagenet/val/ && cd imagenet/val && tar -xf ILSVRC2012_img_val.tar && rm -f ILSVRC2012_img_val.tar
 # get script from soumith and run; this script creates all class directories and moves images into corresponding directories
 wget -qO- https://raw.githubusercontent.com/soumith/imagenetloader.torch/master/valprep.sh | bash
 #
